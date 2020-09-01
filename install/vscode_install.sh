ecc3df22b24225cec40ef0b8f6998f29d7ee4746
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-08-31 20:26:07 -0400

    install(vscode): add initital script for installing vscode and extensions

---
+++ install/vscode_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# VSCODE INSTALL
+# Sources:
+# https://code.visualstudio.com/docs/setup/linux
+# https://code.visualstudio.com/docs/editor/extension-gallery#_command-line-extension-management
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+
+declare -a code_extensions
+
+code_extensions=(
+  ms-python.python
+  editorconfig.editorconfig
+  njpwerner.autodocstring
+  lextudio.restructuredtext
+  joaompinto.vscode-graphviz
+)
+
+for ext in "${code_extensions[@]}"
+do
+    code --install-extension "$ext"
+done
+
