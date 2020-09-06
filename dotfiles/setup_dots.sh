aa2735986696b9eced9a6a18c903832bfeca47a0
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-09-06 10:03:54 -0400

    dot(script): add initial script for generating dotfile symlinks

---
+++ dotfiles/setup_dots.sh
@@ -0,0 +1,41 @@
+#1/bin/bash
+
+# Current script requires that we be in the dotfiles working
+# directory when run by bash. This needs to be fixed.
+dotfilesDir=$(pwd)
+
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${1} ]; then
+    # Existing symlink 
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest} 
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+linkDotfile .vimrc
+linkDotfile .bashrc
+linkDotfile .gitconfig
+linkDotfile .tmux.conf
+linkDotfile .inputrc
+linkDotfile .cookiecutterrc
+
+
+
+
+
