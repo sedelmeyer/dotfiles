7b4c117b3683bb017a9b78aacb77d6938165e357
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-09-24 07:13:20 -0400

    install(abcde): draft abcde install script

--- scripts/install_abcde.sh
+++ scripts/install_abcde.sh
@@ -4,23 +4,48 @@
 # bionic apt pkg is out of date and musicbrainz api
 # interface is broken
 
-# first apt-based install dependencies
-# lame
-# normalize-audio
-# cdparanoia
-# cd-discid id3 eyed3
-# libmusicbrainz-discid-perl
-# glycr # not in reqs doc
-# libmojolicious-perl # required for web services
+wget_dir=../downloads/
+ws_pkg_dir=WebService-MusicBrainz-1.0.4
+abcde_pkg_dir=abcde-2.9.3
 
-# download and unpack musikbrainz web servces perl script from fosse repo
-# libwebservice-musicbrainz-perl
-# http://archive.ubuntu.com/ubuntu/pool/universe/libw/libwebservice-musicbrainz-perl/libwebservice-musicbrainz-perl_1.0.4.orig.tar.gz
-# perl Makefile.PL
-# make
-# make test
-# make install
-# install musicbrainz web services perl script from fosse repo
+# first install apt-based install dependencies
+
+declare -a packages
+
+packages=(
+    lame # mp3 support
+    # vorbis-tools
+    # flac
+    normalize-audio
+    cdparanoia
+    cd-discid
+    id3
+    eyed3
+    glycr # not in reqs doc
+    libmusicbrainz-discid-perl
+    # libwebservice-musicbrainz-perl # requires >=1.0.4, not in bionic repo
+    libmojolicious-perl # dependency for libwebservice manual install
+)
+
+for pkg in "${packages[@]}"
+do
+    sudo apt install -y "$pkg"
+done
+
+
+# download and unpack musikbrainz web servces perl script from focal repo
+
+wget http://archive.ubuntu.com/ubuntu/pool/universe/libw/libwebservice-musicbrainz-perl/libwebservice-musicbrainz-perl_1.0.4.orig.tar.gz -P ${wget_dir}
+
+tar -xvzf ${wget_dir}libwebservice-musicbrainz-perl_1.0.4.orig.tar.gz -C ${wget_dir}
+
+(cd ${wget_dir}${ws_pkg_dir} && perl Makefile.PL && make && make test && make install)
+
 
 # download latest abcde package from project website
-# make install
+
+wget https://abcde.einval.com/download/abcde-2.9.3.tar.gz -P ${wget_dir}
+
+tar -xvzf ${wget_dir}${abcde_pkg_dir}.tar.gz -C ${wget_dir}
+
+(cd ${wget_dir}${abcde_pkg_dir} && make install)
