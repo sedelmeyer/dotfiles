f5b893b14ba2cf509c64bf930199bcdab0032b6b
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2021-01-26 20:30:48 -0500

    script(pip): add .profile source fix

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -16,10 +16,14 @@ do
 done
 
 # add python user package path to .profile
+# Set PS1 so that .profile can be sourced in script
+# Why do we do this? See here: https://askubuntu.com/a/896723
+PS1=true
 source ~/.profile
-# IF THAT DOESN'T WORK
-# printf "\n# set PATH so it includes python user packages" >> .profile
-# printf "export PATH=\$PATH:\$HOME/.local/bin" >> .profile
+
+# if that does not add our pip-installed pkgs to path
+# printf "\n# set PATH so it includes python user packages" >> ~/.profile
+# printf "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.profile
 
 # confirm path is set and verify installed versions
 for pypkg in "${python_pkgs[@]}"
