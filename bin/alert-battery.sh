c681e37e685bba24b247fcc83f4d9223a0349705
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2021-02-02 06:42:30 -0500

    bin(bat): fix alert-battery discharge logic

--- bin/alert-battery.sh
+++ bin/alert-battery.sh
@@ -10,16 +10,16 @@ bat_files="/sys/class/power_supply/BAT0"
 bat_status=$(cat ${bat_files}/status)
 capacity=$(cat "${bat_files}/capacity")
 
-if [ ${bat_status}=="Discharging" ]
+if [ ${bat_status} = "Discharging" ]
 then
     if [ ${capacity} -le 10 ]; then
         echo "Battery alert - ${capacity}%"
-        notify-send \
+        notify-send --urgency=critical \
             "EXTREMELY LOW battery" \
             "Only ${capacity}% battery remaining"
     elif [ ${capacity} -le 20 ]; then
         echo "Battery alert - ${capacity}%"
-        notify-send \
+        notify-send --urgency=normal \
             "Low battery" \
             "Only ${capacity}% battery remaining"
     fi
